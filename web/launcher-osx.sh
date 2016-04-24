79ab07e34d85e4f62a077919554b3b52fb4224c5
Varun Ramraj <varun@well.ox.ac.uk>
2016-04-24 21:04:36 +0100

    Fixed more paths in osx launcher.

--- web/launcher-osx.sh
+++ web/launcher-osx.sh
@@ -3,7 +3,7 @@
 export PATH=`pwd`/../python27osx/bin:$PATH
 echo $PATH
 
-export LD_LIBRARY_PATH=$CWD/osx_libs:$LDB_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/osx_libs:$LDB_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 
 python --version
