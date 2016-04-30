d7f8f7edccb37f75ff9d1dd19dc9bf65a9bfff79
Varun Ramraj <varun@well.ox.ac.uk>
2016-04-30 15:31:07 +0100

    Pass full path to correct python for cgi for OSX!!

--- web/launcher-osx.sh
+++ web/launcher-osx.sh
@@ -3,10 +3,8 @@
 export PATH=`pwd`/../python27osx/bin:$PATH
 echo $PATH
 
-export LD_LIBRARY_PATH=`pwd`/osx_libs:$LD_LIBRARY_PATH
-export PYTHONPATH=`pwd`/osx_libs:$PYTHONPATH
-echo $LD_LIBRARY_PATH
-echo $PYTHONPATH
+#export PYTHONPATH=`pwd`/osx_libs:$PYTHONPATH
+#echo $PYTHONPATH
 
-python --version
-python launcher-cherrypy.py
+/usr/bin/env python --version
+/usr/bin/env python launcher-cherrypy.py `pwd`/../python27osx/bin/python 
